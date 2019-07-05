c60ffea841d55b0215c604655a4bd2e6e6c6c799
ian <licitdev@gmail.com>
2019-07-05 11:29:13 +0000

    Added tests.

--- seeded-hashids.js
+++ seeded-hashids.js
@@ -1,54 +1,23 @@
 var ShuffleSeed = require('shuffle-seed');
 var HashIds = require('hashids');
 
+var HexPattern = /^[a-fA-F0-9]+$/;
+var Defaults = {
+  charset: 'abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789', // Removed 1, 0, iI, oO which could look confusing
+  scopes: {},
+  hashLength: 8,
+  shuffleOutput: true,
+  objectId: null
+};
+
 var _initialized = false;
+var _scopes = Defaults.scopes;
+var _charset = Defaults.charset; 
+var _hashLength = Defaults.hashLength;
+var _shuffleOutput = Defaults.shuffleOutput;
+var _objectId = Defaults.objectId;
 
-var _charset = 'abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789'; // Removed 1, 0, iI, oO which could look confusing
-var _hashTemplates = {};
-var _hashTemplateKeys = [];
-var _hashLength = 8;
-var _shuffleHashSaltLength = 4;
-var _objectId = null;
-
-function EncodedSeededHashid(hashScope, hash) {
-  this.getHashScope = function(){
-    return hashScope;
-  }
-  
-  this.toString = function(){
-    return hash.toString();
-  }
-}
-
-function DecodedSeededHashid(hashScope, data) {
-  this.getHashScope = function(){
-    return hashScope;
-  }
-  
-  this.toValue = function(){
-    return data;
-  }
-  
-  this.toString = function(){
-    return data.toString();
-  }
-  
-  this.toObjectId = function(){
-    if(!_objectId){
-      throw new Error('ObjectId variable not configured.');
-    }
-    
-    let objectId = _objectId(data);
-    if(objectId.toString() === data){
-      return objectId;
-    }
-    else{
-      return null;
-    }
-  }
-}
-
-function generateRandomObjectId(){ // Used for testing the ObjectId variable
+function _generateRandomObjectId(){ // Used for testing the ObjectId variable
   var text = "";
   var charList = "0123456789abcdef";
   for(var i=0; i < 24; i++){  
@@ -57,221 +26,290 @@ function generateRandomObjectId(){ // Used for testing the ObjectId variable
   return text;
 }
 
-function createHasher(secret){
-  return new HashIds(secret, _hashLength, getShuffledCharset(secret));
+function _createHasher(salt){
+  return new HashIds(salt, _hashLength, _getShuffledCharset(salt));
 }
 
-function shuffleSeededString(str, seed) {
+function _shuffleSeededString(str, seed) {
 	return ShuffleSeed.shuffle(str.split(''), seed).join('');
 }
 
-function unshuffleSeededString(str, seed) {
+function _unshuffleSeededString(str, seed) {
 	return ShuffleSeed.unshuffle(str.split(''), seed).join('');
 }
 
-function getShuffledCharset(seed) {
-	return shuffleSeededString(_charset, seed);
+function _getShuffledCharset(seed) {
+	return _shuffleSeededString(_charset, seed);
 }
 
-function doDecode(useHex, scope, hash, seed = '') {
-	let selectedHasher = _hashTemplates[scope];
-  
-  if(!selectedHasher){
-    throw new Error('Missing hash scope.');
-  }
-  
-  if(hash instanceof EncodedSeededHashid){
-    hash = hash.toString();
-  }
-  
-	if (!hash || typeof hash !== 'string') {
-    throw new Error('Missing hash.');
-	}
-  
-	hash = unshuffleSeededString(hash, selectedHasher.salt.substring(0, _shuffleHashSaltLength));
-  
-	let decodedHash;
-
-  if (useHex) {
-		// Hex based hash
-		let decoded = selectedHasher.decodeHex(hash);
-		if (typeof decoded === 'string') {
-			decodedHash = decoded;
-		}
-		else {
-			decodedHash = '';
-		}
-	}
-	else {
-		// Count based hash
-		let decoded = selectedHasher.decode(hash);
-		if (decoded instanceof Array && decoded.length > 0) {
-			decodedHash = decoded[0] + '';
-		}
-		else {
-			decodedHash = '';
-		}
-	}
-
-	// Must have seed and decoded hash cannot be empty string
-	if (seed && decodedHash) {
-		decodedHash = unshuffleSeededString(decodedHash, scope + seed);
-	}
-  
-  if(!useHex){
-    decodedHash = parseInt(decodedHash);
-  }
-  
-  return new DecodedSeededHashid(scope, decodedHash);
-}
-
-function doEncode(useHex, scope, data, seed = '') {
-	let selectedHasher = _hashTemplates[scope];
+function _encode(useHex, scope, data, seed) {
+	var selectedHasher = _scopes[scope];
 
   if(!selectedHasher){
-    throw new Error('Missing hash scope.');
+    throw new Error('Missing scope.');
   }
   
-  if(data instanceof DecodedSeededHashid){
-    data = data.toString();
+  if(useHex){
+    if (!data || typeof data !== 'string' || !HexPattern.test(data)) {
+      throw new Error('Missing data, must be a hex string.');
+    }
+  }
+  else{
+    if (!data || typeof data !== 'number' || parseInt(data) < 0) {
+      throw new Error('Missing data, must be a positive number.');
+    }
   }
   
-  if (!data || !data.toString || typeof data.toString !== "function") {
-    throw new Error('Missing data.');
+  if (seed && typeof seed !== 'string') {
+    throw new Error('Invalid seed, must be a string.');
 	}
 
 	data = data.toString(); // Convert to string so it could be shuffled
 
 	// Must have seed and data cannot be empty string
 	if (seed && data) {
-		data = shuffleSeededString(data, scope + seed);
+		data = _shuffleSeededString(data, scope + seed);
 	}
 
+  var hash;
+  
   if(useHex){
 		// Hex based hash
-    return new EncodedSeededHashid(scope, shuffleSeededString(
-      selectedHasher.encodeHex(data),
-      selectedHasher.salt.substr(0, _shuffleHashSaltLength)
-    ));
+    hash = selectedHasher.encodeHex(data);
   }
   else{
 		// Count based hash
-    return new EncodedSeededHashid(scope, shuffleSeededString(
-      selectedHasher.encode(data),
-      selectedHasher.salt.substr(0, _shuffleHashSaltLength)
-    ));
+    hash = selectedHasher.encode(data);
   }
+  
+  if(!hash){ // Crossed Hashids limit of hashes that can be generated with charset
+    throw new Error('Input data too huge, unable to encoded.');
+  }
+  
+  if(_shuffleOutput){
+    return _shuffleSeededString(hash, selectedHasher.alphabet);
+  }
+
+  return hash;
 }
 
-function requireUninitialized(){
+function _decode(useHex, scope, hash, seed) {
+	var selectedHasher = _scopes[scope];
+  
+  if(!selectedHasher){
+    throw new Error('Missing scope.');
+  }
+  
+	if (!hash || typeof hash !== 'string') {
+    throw new Error('Missing hash, must be a string.');
+	}
+  
+  if (seed && typeof seed !== 'string') {
+    throw new Error('Invalid seed, must be a string.');
+	}
+  
+  if(_shuffleOutput){
+    hash = _unshuffleSeededString(hash, selectedHasher.alphabet);
+  }
+  
+	var data;
+
+  if (useHex) {
+		// Hex based hash
+		data = selectedHasher.decodeHex(hash);
+		if (typeof data !== 'string') {
+			data = '';
+		}
+	}
+	else {
+		// Count based hash
+		data = selectedHasher.decode(hash);
+		if (data instanceof Array && data.length > 0) {
+			data = data[0] + '';
+		}
+		else {
+			data = '';
+		}
+	}
+
+	// Must have seed and decoded hash cannot be empty string
+	if (seed && data) {
+		data = _unshuffleSeededString(data, scope + seed);
+	}
+  
+  if(!useHex){
+    data = parseInt(data);
+  }
+  
+  return data;
+}
+
+function _requireUninitialized(){
   if(_initialized){
     throw new Error('Unable to configure after initialization.');
   }
 }
 
-function requireInitialized(){
+function _requireInitialized(){
   if(!_initialized){
     throw new Error('Initialization is required.');
   }
 }
 
-
 module.exports = {
-  setObjectIdVariable: function(objectIdVariable){
-    requireUninitialized();
+  getInitialized: function() {
+    return _initialized;
+  },
+  
+  getScopes: function() {
+    return _scopes;
+  },
+  
+  getCharset: function() {
+    return _charset;
+  },
+  
+  getHashLength: function() {
+    return _hashLength;
+  },
+  
+  getShuffleOutput: function() {
+    return _shuffleOutput;
+  },
+  
+  getObjectId: function() {
+    return _objectId;
+  },
+  
+  encode: function(scope, data, seed) {
+    _requireInitialized();
 
-    if(typeof objectIdVariable !== 'function'){
-      throw new Error('Invalid objectid variable.');
+    return _encode(false, scope, data, seed);
+  },
+
+  encodeHex: function(scope, data, seed) {
+    _requireInitialized();
+
+    return _encode(true, scope, data, seed);
+  },
+
+  decode: function(scope, hash, seed) {
+    _requireInitialized();
+
+    return _decode(false, scope, hash, seed);
+  },
+
+  decodeHex: function(scope, hash, seed) {
+    _requireInitialized();
+
+    return _decode(true, scope, hash, seed);
+  },
+  
+  decodeObjectId: function(scope, hash, seed) {
+    _requireInitialized();
+    
+    if(!_objectId){
+      throw new Error('ObjectId variable not configured.');
     }
-
-    try{
-      let randomObjectId = generateRandomObjectId();
-      if(objectIdVariable(randomObjectId).toString() !== randomObjectId){
-        throw new Error('Invalid objectid variable.');
+    
+    // Do decode as per hex
+    var data = _decode(true, scope, hash, seed);
+    
+    // Cast to ObjectId if is valid
+    if(data && data.length === 24){
+      var objectId = _objectId(data);
+      if(objectId.toString() === data){
+        return objectId;
       }
     }
-    catch(err){
-      throw new Error('Invalid objectid variable.');
+    
+    return null;
+  },
+
+  initialize: function(options){ // [{scope: String, salt: String}, ...]
+    _requireUninitialized();
+    
+    if(options.hashLength !== undefined){
+      options.hashLength = parseInt(options.hashLength);
+
+      if(isNaN(options.hashLength) || options.hashLength < 0){
+        throw new Error('Invalid hashLength, must be a positive number.');
+      }
+
+      _hashLength = options.hashLength;
+    }
+    
+    if(options.shuffleOutput !== undefined){
+      _shuffleOutput = !!options.shuffleOutput;
+    }
+    
+    if(options.charset !== undefined){
+      if(typeof options.charset !== 'string'){
+        throw new Error('Invalid charset, must a string with 16 or more unique characters.');
+      }
+
+      var uniqueCharacters = '';
+      for(var x = 0; x < options.charset.length; x++){
+        if(uniqueCharacters.indexOf(options.charset[x]) === -1){
+          uniqueCharacters += options.charset[x];
+        }
+      }
+      
+      if(uniqueCharacters.length < 16){ // Required by hashids
+        throw new Error('Invalid charset, must a string with 16 or more unique characters.');
+      }
+
+      _charset = uniqueCharacters;
+    }
+    
+    if(options.objectId !== undefined){
+      if(typeof options.objectId !== 'function'){
+        throw new Error('Invalid objectId, should pass in Mongoose or MongoDB ObjectId variable.');
+      }
+
+      try{
+        var randomObjectId = _generateRandomObjectId();
+        if(options.objectId(randomObjectId).toString() !== randomObjectId){
+          throw new Error('Invalid objectId, should pass in Mongoose or MongoDB ObjectId variable.');
+        }
+      }
+      catch(err){
+        throw new Error('Invalid objectId, should pass in Mongoose or MongoDB ObjectId variable.');
+      }
+
+      _objectId = options.objectId;
+    }
+    
+    if(!options.scopes || !(options.scopes instanceof Array) || options.scopes.length == 0){
+      throw new Error('Invalid scopes. Must be an array of {scope, salt} which are strings.');
+    }
+    
+    var scopes = {};
+    for(var i = 0; i < options.scopes.length; i++){
+      if(!options.scopes[i].scope ||
+         typeof options.scopes[i].scope !== 'string' ||
+         !options.scopes[i].salt ||
+         typeof options.scopes[i].salt !== 'string'){
+        throw new Error('Invalid scopes. Must be an array of {scope, salt} which are strings.');
+      }
+      if(scopes[options.scopes[i].scope]){
+        throw new Error('Scope exists. Please ensure that scopes are unique.');
+      }
+      
+      scopes[options.scopes[i].scope] = _createHasher(options.scopes[i].salt);
     }
 
-    _objectId = objectIdVariable; 
-  },
-  
-  setHashLength: function(length) {
-    requireUninitialized();
-
-    length = parseInt(length);
-
-    if(isNaN(length) || length < 0){
-      throw new Error('Invalid hash length, must be a number.');
-    }
-
-    _hashLength = length;
-  },
-  
-  setCharset: function(newCharset) {
-    requireUninitialized();
-
-    if(typeof newCharset !== 'string' || newCharset.length < 16){ // Required by hashids
-      throw new Error('Invalid charset, must be > 16 unique characters.');
-    }
-
-    _charset = newCharset;
-  },
-  
-  setShuffleHashSaltLength: function(length) {
-    requireUninitialized();
-
-    length = parseInt(length);
-
-    if(isNaN(length) || length < 1){
-      throw new Error('Invalid shuffle hash salt length, must be a number > 0.');
-    }
-
-    _shuffleHashSaltLength = length;
-  },
-  
-  encode: function(scope, data, seed = '') {
-    requireInitialized();
-
-    return doEncode(false, scope, data, seed);
-  },
-
-  encodeHex: function(scope, data, seed = '') {
-    requireInitialized();
-
-    return doEncode(true, scope, data, seed);
-  },
-
-  decode: function(scope, hash, seed = '') {
-    requireInitialized();
-
-    return doDecode(false, scope, hash, seed);
-  },
-
-  decodeHex: function(scope, hash, seed = '') {
-    requireInitialized();
-
-    return doDecode(true, scope, hash, seed);
-  },
-
-  initialize: function(hashTemplates){ // [{scope: String, salt: String}, ...]
-    requireUninitialized();
-
-    if(!hashTemplates || !(hashTemplates instanceof Array) || hashTemplates.length == 0){
-      throw new Error('Invalid hash templates.');
-    }
-
-    var templates = {};
-    for(var i = 0; i < hashTemplates.length; i++){
-      templates[hashTemplates[i].scope] = createHasher(hashTemplates[i].salt);
-    }
-
-    _hashTemplates = Object.freeze(templates);
-    _hashTemplateKeys = Object.keys(_hashTemplates);
+    _scopes = Object.freeze(scopes);
 
     _initialized = true;
+  },
+  
+  reset: function(){
+    _charset = Defaults.charset; 
+    _scopes = Defaults.scopes;
+    _hashLength = Defaults.hashLength;
+    _shuffleOutput = Defaults.shuffleOutput;
+    _objectId = Defaults.objectId;
+    _initialized = false;
   }
 }
-
-console.log('loaded...')
---
+++ tests/test.js
@@ -0,0 +1,427 @@
+var seededHashids = require('../seeded-hashids');
+var assert = require('chai').assert;
+
+var defaults = {
+  scopes: [
+    {scope: 'user', salt: 'abcd'},
+    {scope: 'profile', salt: '1234'},
+  ],
+  objectId: require('mongoose').Types.ObjectId, // Can also use require('mongodb').ObjectId
+  hashLength: 8,
+  charset: '1234567890abcdef'
+};
+
+describe('when uninitialized', () => {
+  
+	it('should throw an error for .encode', () => {
+		assert.throws(() => {
+      seededHashids.encode();
+		});
+	});
+  
+  it('should throw an error for .encodeHex', () => {
+		assert.throws(() => {
+      seededHashids.encodeHex();
+		});
+	});
+  
+  it('should throw an error for .decode', () => {
+		assert.throws(() => {
+      seededHashids.decode();
+		});
+	});
+  
+  it('should throw an error for .decodeHex', () => {
+		assert.throws(() => {
+      seededHashids.decodeHex();
+		});
+	});
+  
+  it('should throw an error for .decodeObjectId', () => {
+		assert.throws(() => {
+      seededHashids.decodeObjectId();
+		});
+	});
+  
+  it('should throw an error for .decodeObjectId', () => {
+		assert.throws(() => {
+      seededHashids.decodeObjectId();
+		});
+	});
+
+});
+
+describe('when initializing', () => {
+  afterEach(() => {
+    seededHashids.reset();
+  });
+  
+  it('should not throw an error if reset', () => {
+		assert.doesNotThrow(() => {
+      seededHashids.reset();
+		});
+	});
+  
+	it('should throw an error if missing scopes', () => {
+		assert.throws(() => {
+      seededHashids.initialize({});
+		});
+    assert.throws(() => {
+      seededHashids.initialize({scopes: []});
+		});
+	});
+  
+  it('should throw an error if scopes are invalid', () => {
+		assert.throws(() => {
+      seededHashids.initialize({scopes: [{scope: 'test', salt: 'salt'}, {scope: 123, salt: 'salt'}]});
+		});
+    assert.throws(() => {
+      seededHashids.initialize({scopes: [{scope: 'test', salt: 'salt'}, {scope: 'test2', salt: 555}]});
+		});
+	});
+  
+  it('should throw an error if there are duplicated scopes', () => {
+		assert.throws(() => {
+      seededHashids.initialize({scopes: [{scope: 'test', salt: 'salt'}, {scope: 'test', salt: 'salt'}]});
+		});
+	});
+  
+  it('should throw an error if invalid hashLength is passed', () => {
+		assert.throws(() => {
+      seededHashids.initialize({scopes: defaults.scopes, hashLength: -1});
+		});
+    assert.throws(() => {
+      seededHashids.initialize({scopes: defaults.scopes, hashLength: ''});
+		});
+    assert.throws(() => {
+      seededHashids.initialize({scopes: defaults.scopes, hashLength: {}});
+		});
+	});
+  
+  it('should throw an error if invalid charset is passed', () => {
+		assert.throws(() => {
+      seededHashids.initialize({scopes: defaults.scopes, charset: '1234567890'});
+		});
+    assert.throws(() => {
+      seededHashids.initialize({scopes: defaults.scopes, charset: 1234567890});
+		});
+    assert.throws(() => {
+      seededHashids.initialize({scopes: defaults.scopes, charset: {}});
+		});
+	});
+  
+  it('should throw an error if invalid objectid is passed', () => {
+		assert.throws(() => {
+      seededHashids.initialize({scopes: defaults.scopes, objectId: ''});
+		});
+    assert.throws(() => {
+      seededHashids.initialize({scopes: defaults.scopes, objectId: {}});
+		});
+    assert.throws(() => {
+      seededHashids.initialize({scopes: defaults.scopes, objectId: function(v){return null}});
+		});
+	});
+  
+  it('should not throw an error if valid initialization with objectId', () => {
+		assert.doesNotThrow(() => {
+      seededHashids.initialize(defaults);
+		});
+	});
+  
+  it('should not throw an error if valid initialization without objectId', () => {
+		assert.doesNotThrow(() => {
+      var temp = defaults.objectId;
+      delete defaults.objectId;
+      seededHashids.initialize(defaults);
+      defaults.objectId = temp;
+		});
+	});
+  
+});
+
+
+describe('when initialized', () => {
+  
+  before(() => {
+    seededHashids.reset();
+    seededHashids.initialize(defaults);
+  });
+  
+	it('should throw an error for .initialize', () => {    
+		assert.throws(() => {
+      seededHashids.initialize();
+		});
+	});
+
+  describe('when using .encode', () => {
+
+    it('should throw an error if missing / invalid scope', () => {
+      assert.throws(() => {
+        seededHashids.encode('fake', 123);
+      });
+      assert.throws(() => {
+        seededHashids.encode({}, 123);
+      });
+      assert.throws(() => {
+        seededHashids.encode(123, 123);
+      });
+    });
+    
+    it('should throw an error if invalid data', () => {
+      assert.throws(() => {
+        seededHashids.encode('user', {});
+      });
+      assert.throws(() => {
+        seededHashids.encode('user', 'fake');
+      });
+      assert.throws(() => {
+        seededHashids.encode('user', -1);
+      });
+    });
+    
+    it('should throw an error if invalid seed', () => {
+      assert.throws(() => {
+        seededHashids.encode('user', 123, {});
+      });
+      assert.throws(() => {
+        seededHashids.encode('user', 123, 123);
+      });
+    });
+    
+    it('should throw an error if data too big be encoded', () => {
+      assert.throws(() => {
+        seededHashids.encode('user', 999999999999999999999);
+      });
+    });
+
+  });
+  
+  describe('when using .encodeHex', () => {
+
+    it('should throw an error if missing / invalid scope', () => {
+      assert.throws(() => {
+        seededHashids.encodeHex('fake', 'abcdef1234567890');
+      });
+      assert.throws(() => {
+        seededHashids.encodeHex({}, 'abcdef1234567890');
+      });
+      assert.throws(() => {
+        seededHashids.encodeHex(123, 'abcdef1234567890');
+      });
+    });
+    
+    it('should throw an error if invalid data', () => {
+      assert.throws(() => {
+        seededHashids.encodeHex('user', {});
+      });
+      assert.throws(() => {
+        seededHashids.encodeHex('user', 'fake');
+      });
+      assert.throws(() => {
+        seededHashids.encodeHex('user', -1);
+      });
+      assert.throws(() => {
+        seededHashids.encodeHex('user', 123);
+      });
+    });
+    
+    it('should throw an error if invalid seed', () => {
+      assert.throws(() => {
+        seededHashids.encodeHex('user', 'abcdef1234567890', {});
+      });
+      assert.throws(() => {
+        seededHashids.encodeHex('user', 'abcdef1234567890', 123);
+      });
+    });
+    
+  });
+  
+  describe('when using .decode', () => {
+
+    it('should throw an error if missing / invalid scope', () => {
+      assert.throws(() => {
+        seededHashids.decode('fake', 'hash');
+      });
+      assert.throws(() => {
+        seededHashids.decode({}, 'hash');
+      });
+      assert.throws(() => {
+        seededHashids.decode(123, 'hash');
+      });
+    });
+    
+    it('should throw an error if invalid data', () => {
+      assert.throws(() => {
+        seededHashids.decode('user', {});
+      });
+      assert.throws(() => {
+        seededHashids.decode('user', 123);
+      });
+    });
+    
+    it('should throw an error if invalid seed', () => {
+      assert.throws(() => {
+        seededHashids.decode('user', 'hash', {});
+      });
+      assert.throws(() => {
+        seededHashids.decode('user', 'hash', 123);
+      });
+    });
+    
+  });
+  
+  describe('when using .decodeHex', () => {
+
+    it('should throw an error if missing / invalid scope', () => {
+      assert.throws(() => {
+        seededHashids.decodeHex('fake', 'hash');
+      });
+      assert.throws(() => {
+        seededHashids.decodeHex({}, 'hash');
+      });
+      assert.throws(() => {
+        seededHashids.decodeHex(123, 'hash');
+      });
+    });
+    
+    it('should throw an error if invalid data', () => {
+      assert.throws(() => {
+        seededHashids.decodeHex('user', {});
+      });
+      assert.throws(() => {
+        seededHashids.decodeHex('user', 123);
+      });
+    });
+    
+    it('should throw an error if invalid seed', () => {
+      assert.throws(() => {
+        seededHashids.decodeHex('user', 'hash', {});
+      });
+      assert.throws(() => {
+        seededHashids.decodeHex('user', 'hash', 123);
+      });
+    });
+    
+  });
+  
+  describe('when using .decodeObjectId', () => {
+
+    it('should throw an error if missing / invalid scope', () => {
+      assert.throws(() => {
+        seededHashids.decodeObjectId('fake', 'hash');
+      });
+      assert.throws(() => {
+        seededHashids.decodeObjectId({}, 'hash');
+      });
+      assert.throws(() => {
+        seededHashids.decodeObjectId(123, 'hash');
+      });
+    });
+    
+    it('should throw an error if invalid data', () => {
+      assert.throws(() => {
+        seededHashids.decodeObjectId('user', {});
+      });
+      assert.throws(() => {
+        seededHashids.decodeObjectId('user', 123);
+      });
+    });
+    
+    it('should throw an error if invalid seed', () => {
+      assert.throws(() => {
+        seededHashids.decodeObjectId('user', 'hash', {});
+      });
+      assert.throws(() => {
+        seededHashids.decodeObjectId('user', 'hash', 123);
+      });
+    });
+    
+  });
+  
+});
+
+
+
+describe('when initialized without objectId', () => {
+  
+  before(() => {
+    seededHashids.reset();
+    var temp = defaults.objectId;
+    delete defaults.objectId;
+    seededHashids.initialize(defaults);
+    defaults.objectId = temp;
+  });
+  
+  it('should throw an error for .initialize', () => {
+		assert.throws(() => {
+      seededHashids.initialize();
+		});
+	});
+  
+  it('should throw an error for .decodeObjectId', () => {
+		assert.throws(() => {
+      seededHashids.decodeObjectId('user', 'hash');
+		});
+	});
+  
+});
+
+describe('when encoding and decoding', () => {
+  
+  before(() => {
+    seededHashids.reset();
+    seededHashids.initialize(defaults);
+  });
+  
+  it('should run .encode and .decode correctly without salt', () => {
+    var number = 12345678;
+    var encoded = seededHashids.encode('user', number);
+    var decoded = seededHashids.decode('user', encoded);
+    assert.deepEqual(number, decoded);
+	});
+  
+  it('should run .encode and .decode correctly with salt', () => {
+    var number = 12345678;
+    var salt = 'somesalt';
+    var encoded = seededHashids.encode('user', number, salt);
+    var decoded = seededHashids.decode('user', encoded, salt);
+    assert.deepEqual(number, decoded);
+	});
+  
+  it('should run .encodeHex and .decodeHex correctly without salt', () => {
+    var hex = 'abcdef1234567890';
+    var encoded = seededHashids.encodeHex('user', hex);
+    var decoded = seededHashids.decodeHex('user', encoded);
+    assert.deepEqual(hex, decoded);
+	});
+  
+  it('should run .encodeHex and .decodeHex correctly with salt', () => {
+    var hex = 'abcdef1234567890';
+    var salt = 'somesalt';
+    var encoded = seededHashids.encodeHex('user', hex, salt);
+    var decoded = seededHashids.decodeHex('user', encoded, salt);
+    assert.deepEqual(hex, decoded);
+	});
+  
+  it('should run .encodeHex and .decodeObjectId correctly without salt', () => {
+    var hex = 'abcdef1234567890abcdef12';
+    var encoded = seededHashids.encodeHex('user', hex);
+    var decoded = seededHashids.decodeObjectId('user', encoded);
+    if(decoded){
+      decoded = decoded.toString()
+    }
+    assert.deepEqual(hex, decoded);
+	});
+  
+  it('should run .encodeHex and .decodeObjectId correctly with salt', () => {
+    var hex = 'abcdef1234567890abcdef12';
+    var salt = 'somesalt';
+    var encoded = seededHashids.encodeHex('user', hex, salt);
+    var decoded = seededHashids.decodeObjectId('user', encoded, salt);
+    if(decoded){
+      decoded = decoded.toString()
+    }
+    assert.deepEqual(hex, decoded);
+	});
+  
+});
